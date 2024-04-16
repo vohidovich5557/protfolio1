276bc2d0fa456b71d58df1666da587241a3c13f4
vohidovich5557 <shokhuk006@gmail.com>
2024-04-16 19:44:04 +0500

    protfolio2

--- src/pages/project-detail/index.jsx
+++ src/pages/project-detail/index.jsx
@@ -16,61 +16,61 @@ export const ProjectDetail = () => {
             {result.title}
           </span>
         </h2>
-        <div className="flex items-center mb-[60px] justify-center gap-[50px]">
+        <div className="flex flex-col lg:flex-row items-center mb-[60px] lg:justify-center gap-[50px]">
           <a target="_blank" href={result.link}>
-            <div className="w-[570px] shadow-lg shadow-gray-400 ">
+            <div className="w-[300px] lg:w-[570px] shadow-lg shadow-gray-400 ">
               <img src={result.img} alt="" />
             </div>
           </a>
-          <div className="w-[570px] flex flex-col items-start">
+          <div className="w-[570px] flex flex-col items-center lg:items-start">
             <div className="w-[100%] h-[3px] mb-[30px] bg-gray-300"></div>
-            <p className="text-xl font-bold text-blue-950 mb-[10px]">
+            <p className="text-md lg:text-xl w-[350px] md:w-auto font-bold text-blue-950 mb-[10px]">
               {result.comment1}
             </p>
             <div className="w-[100%] h-[3px] mb-[30px] bg-gray-300"></div>
-            <p className="text-xl font-bold text-blue-950 mb-[10px]">
+            <p className="text-md lg:text-xl w-[350px] md:w-auto font-bold text-blue-950 mb-[10px]">
               {result.comment2}
             </p>
             <div className="w-[100%] h-[3px] mb-[30px] bg-gray-300"></div>
-            <p className="text-xl font-bold text-blue-950 mb-[10px]">
+            <p className="text-md lg:text-xl w-[350px] md:w-auto font-bold text-blue-950 mb-[10px]">
               {result.comment3}
             </p>
             <div className="w-[100%] h-[3px] mb-[30px] bg-gray-300"></div>
-            <p className="text-xl font-bold text-blue-950 mb-[10px]">
+            <p className="text-md lg:text-xl w-[350px] md:w-auto font-bold text-blue-950 mb-[10px]">
               {result.comment4}
             </p>
             <div className="w-[100%] h-[3px] mb-[30px] bg-gray-300"></div>
-            <p className="text-xl font-bold text-blue-950 mb-[10px]">
+            <p className="text-md lg:text-xl w-[350px] md:w-auto font-bold text-blue-950 mb-[10px]">
               Loyihada BackEnd qismiham mavjud. Back End ni Front qismi bilan
               ulagan holda Ma`lumot olish qo`shish va ochirish kabi xususiyatlar
               ishlatilgan.
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-start">
-          <h2 className="text-4xl font-bold text-red-600 mb-[20px]">
+        <div className="flex flex-col items-center lg:items-start">
+          <h2 className="text-2xl lg:text-4xl font-bold text-red-600 mb-[20px]">
             Ishlatilgan Toolar
           </h2>
           <div className="flex flex-col">
             <div className="w-[600px] ml-auto mr-auto mb-[10px] h-[2px] bg-gray-400"></div>
-            <div className="flex items-center mb-[20px] gap-[20px]">
-              <h3 className="text-2xl font-medium text-blue-500">
+            <div className="flex flex-col md:flex-row lg:flex-row items-center mb-[20px] gap-[20px]">
+              <h3 className="text-xl md:text-2xl lg:text-2xl font-medium text-blue-500">
                 Funksiyonalar Tarafi uchun:
               </h3>
               <p>
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   React Js
                 </span>{" "}
                 ||{" "}
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   Redux
                 </span>{" "}
                 ||{" "}
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   React Query
                 </span>{" "}
                 ||{" "}
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   API Fetching
                 </span>{" "}
               </p>
@@ -78,24 +78,24 @@ export const ProjectDetail = () => {
           </div>
           <div className="flex flex-col">
             <div className="w-[600px] ml-auto mr-auto mb-[10px] h-[2px] bg-gray-400"></div>
-            <div className="flex items-center mb-[20px] gap-[20px]">
-              <h3 className="text-2xl font-medium text-blue-500">
+            <div className="flex flex-col md:flex-row lg:flex-row items-center mb-[20px] gap-[20px]">
+              <h3 className="text-xl lg:text-2xl md:text-2xl font-medium text-blue-500">
                 Design Tarafi uchun:{" "}
               </h3>
               <p>
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   Tailwind Css
                 </span>{" "}
                 ||{" "}
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   Headless UI
                 </span>{" "}
                 ||{" "}
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   Slick
                 </span>{" "}
                 ||{" "}
-                <span className="text-xl font-medium text-yellow-600">
+                <span className="text-md md:text-xl lg:text-xl font-medium text-yellow-600">
                   React Skeleton
                 </span>{" "}
               </p>
